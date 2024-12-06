37a214c0cd42f98ce263d9f91d4cd149f391f9a8
Quang <qth9368@rit.edu>
2024-12-06 08:09:10 -0500

    implemented game class in models.py

--- backend/models.py
+++ backend/models.py
@@ -4,3 +4,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     steam_id = db.Column(db.String(20), unique=True, nullable=True)
     username = db.Column(db.String(100))
+    profile_url = db.Column(db.String(200))
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
