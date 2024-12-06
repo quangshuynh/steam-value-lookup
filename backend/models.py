6817a5dbeebef94c894d0507d384462ccf3e3e61
Quang <qth9368@rit.edu>
2024-12-06 08:06:31 -0500

    implemented user class in models.py

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,6 @@
-"""
-todo: Define SQLAlchemy models for users, games, and inventories.
-"""
+from database import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    steam_id = db.Column(db.String(20), unique=True, nullable=True)
+    username = db.Column(db.String(100))
