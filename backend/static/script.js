33a38d841aaeb0f2a6ed964a62107c69f3ccc702
Quang <qth9368@rit.edu>
2024-12-07 10:04:50 -0500

    sorted button changes the text on top

--- backend/static/script.js
+++ backend/static/script.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 function sortTable(order) {
     const table = document.querySelector('.games-list table tbody');
     const rows = Array.from(table.querySelectorAll('tr'));
+    const sortingMessage = document.getElementById('sorting-message');
 
     rows.sort((a, b) => {
         const nameA = a.cells[1].textContent.trim().toLowerCase();
@@ -19,12 +20,16 @@ function sortTable(order) {
         const hoursB = parseFloat(b.cells[2].textContent.trim());
 
         if(order === 'hours-desc') {
+            sortingMessage.textContent = 'Here are your Steam games sorted by playtime (highest to lowest)';
             return hoursB - hoursA;
         } else if(order === 'hours-asc') {
+            sortingMessage.textContent = 'Here are your Steam games sorted by playtime (lowest to highest)';
             return hoursA - hoursB;
         } else if(order === 'alpha-desc') {
+            sortingMessage.textContent = 'Here are your Steam games sorted alphabetically (Z to A)';
             return nameB.localeCompare(nameA);
         } else if(order === 'alpha-asc') {
+            sortingMessage.textContent = 'Here are your Steam games sorted alphabetically (A to Z)';
             return nameA.localeCompare(nameB);
         }
     });
